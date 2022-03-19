b224160c02f1e75da704b55b36e70077e7d610db
JC Bince <jbince1@nait.ca>
2022-03-19 15:39:17 -0600

    toastify and credentials work, tested, to dashboard page

--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -8,10 +8,16 @@ const LoginPageStyles = styled.section `
 	h1 {
 		font-size: 2rem;
 	}
+
+	.Toastify__toast{
+		background-color: red:
+		color:white;
+	}
 `
 const FormControl = styled.div`
 		margin-bottom:1rem;
 		justify-content: center;
 	}
+	
 `
 export { LoginPageStyles, FormControl }
