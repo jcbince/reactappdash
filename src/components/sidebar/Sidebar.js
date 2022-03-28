e32a436a73419844d1655f8ea7be46ba7420f5a9
JC Bince <jbince1@nait.ca>
2022-03-27 18:29:45 -0600

    had some issue with signing out. Added a redirect. Hopefully signout isnt going to screw me

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import { signOut } from 'firebase/auth'
+import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from 'libs/firebase'
 
 
 import { SideBarStyles} from './styles'
 
-import {MdDashboard} from "react-icons/md";
 import { Button } from 'ui/buttons'
 import ProductOptions from './ProductOptions'
 
@@ -15,24 +15,44 @@ import ProductOptions from './ProductOptions'
 const SideBar = (props) => {
 
 	function onLogoutRequest(e) {
-		signOut(auth)
+		setIsUser(false)
+		navigator('/')
+		
 	}
-  	return (
-		<SideBarStyles>
-			<header>
-				<MdDashboard size="1.75rem"/>
-				<h1>SideBar</h1>
-				<nav>
-				<ProductOptions/>
-				
-				</nav>
-			</header>
 
-			
-			<Button onClick={onLogoutRequest}>Logout</Button>
-			
-		</SideBarStyles>
-  )
+	const [isUser, setIsUser] = useState(false)
+	const navigator = useNavigate();
+
+	   onAuthStateChanged(auth, (user)=>{
+		   if(user){
+			   setIsUser(true)
+		   }else{
+			  setIsUser(false)
+			  navigator('/')
+		   }
+	   })
+
+	
+	  if(isUser){
+		return (
+			<SideBarStyles>
+				<header>
+					<h1>SideBar</h1>
+					<nav>
+					<ProductOptions/>
+					
+					</nav>
+				</header>
+	
+				
+				<Button onClick={onLogoutRequest}>Logout</Button>
+				
+			</SideBarStyles>
+	  )
+	  }else{
+		  return null
+	  }
+  	
 }
 
 export default SideBar
