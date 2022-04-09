cde536e655a21dcc151c0d8cb3e8d39863858c58
JC Bince <jbince1@nait.ca>
2022-04-08 21:46:00 -0600

    got the login and add screen to work, the product editor I was working on in the dark appeared

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import {Route, Routes } from 'react-router-dom'
  
-import { DashBoardPage, LoginPage, PageNotFound} from './pages'
-import { AllProductsPanel, DeleteProductPanel, UpdateProductPanel } from 'components/panels';
-import AddProductPanel from 'components/panels/DeleteProductPanel';
+import {LoginPage, PageNotFound, DashBoardPage} from './pages'
+import { AllProductsPanel} from 'components/panels';
+import AddProductPanel from 'components/panels/AddProductPanel';
 
 function App() {
   return (
     <>
-    <Routes>
-           <Route path="/" index  element={<LoginPage/>}/>
+     <Routes>
+           <Route path="/"  element={<LoginPage/>}/>
            <Route path="dashboard"  element={<DashBoardPage/>} >
                 <Route index   element={<AllProductsPanel title="All Products"/>}/>
-                <Route path="add"   element={<AddProductPanel title="Add New Product "/>}/> 
-                <Route path="update"   element={<UpdateProductPanel title="Update Product "/>}/> 
-                <Route path="delete"   element={<DeleteProductPanel title="Delete Product "/>}/> 
-
+                <Route path="add"   element={<AddProductPanel title="Add New Product "/>}/>
        
             </Route>
         
